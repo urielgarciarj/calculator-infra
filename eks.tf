8093de5135216af86ea6e679669558efecca6316
urielgarciarj@gmail.com <uriel.garcia@bringitps.com>
2025-07-03 19:17:33 -0600

    Adding initial configuration of terraform
    Signed-off-by: urielgarciarj@gmail.com <uriel.garcia@bringitps.com>

---
+++ eks.tf
@@ -0,0 +1,44 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name                    = "calculator-eks"
+  cluster_version                 = "1.27"
+  vpc_id                          = module.vpc.vpc_id
+  subnet_ids                      = module.vpc.private_subnets
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  manage_aws_auth_configmap = true
+
+  eks_managed_node_groups = {
+    node-group = {
+      desired_capacity = 1
+      max_capacity     = 2
+      min_capacity     = 1
+      instance_types   = ["t3.small"]
+
+      tags = {
+        Name        = "calculator-node-group"
+        Environment = "dev"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "calculator-eks"
+    Environment = "dev"
+  }
+}
